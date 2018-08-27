b2bdf7006e34f916bff6eda2b618b2f2bb7d7921
cjs10lib <34670425+cjs10lib@users.noreply.github.com>
2018-08-27 15:30:50 +0000

    Creating layout components (admin, auth, & general layouts)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { GeneralLayoutComponent } from './layouts/general-layout/general-layout.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminLayoutComponent,
+    GeneralLayoutComponent,
+    AuthLayoutComponent
   ],
   imports: [
     BrowserModule,
