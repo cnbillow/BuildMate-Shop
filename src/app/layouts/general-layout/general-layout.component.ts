81fb7d965fc6f19747373677f0df9fe2d84abb56
cjs10lib <34670425+cjs10lib@users.noreply.github.com>
2018-09-14 05:42:44 +0000

    Displaying cart items quantity in general layout

--- src/app/layouts/general-layout/general-layout.component.ts
+++ src/app/layouts/general-layout/general-layout.component.ts
@@ -1,6 +1,9 @@
+import { CartItem } from './../../models/cartItem.model';
+import { Client } from './../../models/client.model';
+import { ClientShoppingCartService } from './../../services/client-shopping-cart.service';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { Subscription, Observable } from 'rxjs';
 import { AuthService } from '../../services/auth.service';
 import { AuthGuard } from '../../services/auth.guard';
 import { AdminAuthGuard } from '../../services/admin-auth.guard';
@@ -15,22 +18,27 @@ export class GeneralLayoutComponent implements OnInit, OnDestroy {
   currentUrl: string;
   clientHeight: number;
 
+  clientCart: Observable<CartItem[]>;
+
   authProviderData = {};
 
   subscription: Subscription;
   authSubscription: Subscription;
 
   constructor(router: Router,
-              public auth: AuthService) {
+              public auth: AuthService,
+              private clientCartService: ClientShoppingCartService) {
     this.subscription = router.events.subscribe((_: NavigationEnd) => this.currentUrl = _.url);
     this.clientHeight = window.innerHeight;
   }
 
-  ngOnInit() {
+  async ngOnInit() {
     this.auth.user$.subscribe(user => {
       if (!user) { return; }
       this.authProviderData = user.providerData[0];
     });
+
+    this.clientCart = (await this.clientCartService.getCart());
   }
 
   ngOnDestroy(): void {
@@ -41,6 +49,10 @@ export class GeneralLayoutComponent implements OnInit, OnDestroy {
    if (this.authSubscription) {
      this.authSubscription.unsubscribe();
    }
+  }
+
+  getCartItemTotal(cart) {
+    return this.clientCartService.getCartTotalItemCount(cart);
   }
 
   signOut() {
